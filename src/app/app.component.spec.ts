47a081e5683b18c8cd64ac6fb64f9140a0f4069f
jsmravliuk <js.7ravliuk@gmail.com>
2020-08-09 20:00:35 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'littleBookStore'`, () => {
+  it(`should have as title 'book-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('littleBookStore');
+    expect(app.title).toEqual('book-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('littleBookStore app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('book-store app is running!');
   });
 });
